2352e3ba148c4ad4dce40451335dc271ef4e8fe7
thiccaxe <51303986+chungfungi@users.noreply.github.com>
2020-10-23 23:48:15 -0700

    changed from cookies to localStorage

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,66 +1,26 @@
-const URLREGEX = /[.:]/g
+//100% tollatyt 1\10 guranted Coopyrte (Ɔ) tjicaxe.bet
+//but seriously most of this shit is copy pasted from w3 schools so feel free to Ctrl+C/Ctrl+V (unless ur on mac lyke a loosur then }{+C and }{+V)
+//anyhow try and read through this anyway gl hf
 
-//General Use Functions
-function delay (URL) {
-    setTimeout( function() { window.location = URL }, 200 );
-}
-//Functions for cookies
 
-function setCookie(name, value, ttl, path="/") {
-    var exdate = new Date();
-    exdate.setDate( exdate.getDate() + ttl);
-    var value = escape( value ) + ( ( ttl == null ) ? "" : "; expires=" + exdate.toUTCString() );
-    document.cookie = name + "=" + value + "; path="+path;
-    console.log("set cookie: " + name + "=" + value + "; path="+path);
-    document.getElementById('devtools').innerHTML+="<p>" + "set cookie: " + name + "=" + value + "; path="+path + "</p>"
-    //document.getElementById('devtools').innerHTML=document.cookie
-    //alert(document.cookie + "\nset cookie: " + name + "=" + value + "; path="+path)
+
+//Functions for localStorage
+
+function setItem(key, val) {
+    localStorage.setItem(key, val);
+    console.log('set', key, val);
 }
 
-function getCookie(name) {
-    var ckname = name+ "=";
-    var cookies = decodeURIComponent(document.cookie).split(';');
-    console.log('Getting Cookies with name: ' + name);
-    var gotCookies = false;
-    for (var i =0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        while (cookie.charAt(0) == ' ') {
-            cookie = cookie.substring(1);
-        }
-        if (cookie.indexOf(ckname) == 0) {
-            console.log("got cookie: " + name + " = " + cookie.substring(ckname.length, cookie.length));
-            document.getElementById('devtools').innerHTML+="<p>got cookie: " + name + " = " + cookie.substring(ckname.length, cookie.length) + "</p>";
-            return cookie.substring(ckname.length, cookie.length);
-            gotCookies = true;
-        }
-    }
-    if (gotCookies==false) {
-        console.log("Found no cookies with name: " + name);
-        document.getElementById('devtools').innerHTML+="<p>Got no cookies with name: " + name + "</p>";
-    }
+function getItem(key) {
+    console.log('got', localStorage.getItem(key));
+    return localStorage.getItem(key);
+    
 }
 
-function setDarkModeCookie() {
-    var currentMode = document.body.classList.value;
-    if (currentMode.includes('dark-mode')) {
-        setCookie("darkmode", "true", 365);
-    } else {
-        setCookie("darkmode", "false", 365);
-    }
+function deleteItem(key) {
+    localStorage.removeItem(key);
 }
 
-function setTabCookie(tabID) {
-    currentPage = window.location.href.split('://')[1].replaceAll("#", '').replaceAll(URLREGEX, "_").replaceAll('/', '_');
-    setCookie("visitedTab_"+currentPage, tabID, 365, window.location.href.replaceAll("#", ''));
-}
-
-function selectTabFromCookie() {
-    currentPage = window.location.href.split('://')[1].replaceAll("#", '').replaceAll(URLREGEX, "_").replaceAll('/', '_');
-    cookie = getCookie("visitedTab_" + currentPage);
-    if (cookie != null) {
-        document.getElementById("TAB:" + cookie).click();
-    }
-}
 
 //Functions for tabs
 function loadHTML(evt, Name) {
@@ -76,24 +36,82 @@ function loadHTML(evt, Name) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    document.getElementById(Name).style.display = "block";
+    document.getElementById('TABCONTENT:' + Name).style.display = "block";
     evt.currentTarget.className += " active"
 
-    setTabCookie(Name);
 }
 
 //functions for the slide in overlay
-
+let overlayOpen = false;
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+    if (overlayOpen == false) {
+        document.getElementById("overlay-container").style.width = "100%";
+        overlayOpen = true;
+    }
+
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+    if (overlayOpen) {
+        document.getElementById("overlay-container").style.width = "0%";
+        overlayOpen = false;
+    }
+
 }
 
 
-function toggleMode() {
-    document.getElementById("darkmodetoggle").innerHTML = document.body.classList.toggle("dark-mode") ? '<i class="material-icons">check_box</i> Dark Mode' : '<i class="material-icons">check_box_outline_blank</i> Dark Mode'
+function toggleTheme(obj = document.getElementById('darkmodetoggle'), text=" Dark Mode") {
+    obj.innerHTML = document.body.classList.toggle("dark-mode") ? '<i class="material-icons">check_box</i>' + text: '<i class="material-icons">check_box_outline_blank</i>' + text;
+    if (theme = getItem('theme') == 'dark') {
+        setItem('theme', 'light');
+    } else {
+        setItem('theme', 'dark');
+    }
 }
 
+
+
+document.addEventListener('readystatechange', event => { 
+
+    // When HTML/DOM elements are ready:
+    if (event.target.readyState === "interactive") {   //does same as:  ..addEventListener("DOMContentLoaded"..
+        
+    }
+
+    // When window loaded ( external resources are loaded too- `css`,`src`, etc...) 
+    if (event.target.readyState === "complete") {
+        //mobile ??? O:
+        var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        if (isMobile == false) {
+            console.log(document.getElementById("close-overlay-mobile").innerHTML = "");
+        } else {
+            console.log(document.getElementById("close-overlay-desktop").innerHTML = "");
+        }
+
+        if (theme = getItem('theme') != null) {
+            if (theme == 'dark') {
+                toggleTheme()
+            }
+        } else {
+            setItem('theme', 'light')
+        }
+    }
+});
+
+ 
+
+//random stuff
+function goTo(str) {
+    str = String(str);
+    if (str.charAt(0) == '/') {
+        if (str != window.location.pathname) {
+            window.location.href = str;
+        } else {
+            closeNav();
+        }
+    } else {
+        if (str == 'Toggle Theme') {
+            closeNav(); toggleTheme();
+        }
+    }
+}
